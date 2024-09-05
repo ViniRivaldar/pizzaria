e7f2b53ed79e344e0316e321645d12c9ca8c6be5
ViniRivaldar <vinicius-rivaldar@outlook.com>
2024-09-05 19:47:19 -0300

    ajustando reducer

--- src/store/modules/auth/reducer.jsx
+++ src/store/modules/auth/reducer.jsx
@@ -29,7 +29,7 @@ export default function reducer(state = initialState, actions){
 
         case types.LOGIN_REQUEST:{
             const newState = {...state}
-            newState.isLoading = true
+            newState.isLoading = false
             return newState
         }
       
