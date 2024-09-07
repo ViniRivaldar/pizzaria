e5ebcb09bc133112a347d6512a2102c3aff488c5
ViniRivaldar <vinicius-rivaldar@outlook.com>
2024-09-07 18:39:11 -0300

    deletando usuario

--- src/page/Register/index.jsx
+++ src/page/Register/index.jsx
@@ -2,12 +2,14 @@ import { useEffect, useState } from 'react'
 import * as yup from 'yup'
 import {toast} from 'react-toastify'
 import { useDispatch, useSelector } from 'react-redux'
+import { get } from 'lodash'
 
 
 import {Container} from '../../styles/GlobalStyles'
 import { Form } from './styled'
 import Loading from '../../components/loading'
 import * as actions from '../../store/modules/auth/actions'
+import axios from '../../services/axios'
 
 export default function Register(){
     const[name, setName] = useState('')
@@ -42,7 +44,7 @@ export default function Register(){
 
         try {
             schema.validateSync({name, phone, email, password},{abortEarly:false})
-            dispatch(actions.registerRequest({name, phone, email, password}))
+            dispatch(actions.registerRequest({ id,name, phone, email, password}))
         } catch (err) {
             if (err instanceof yup.ValidationError) {
                 err.inner.forEach((error) => {
@@ -54,11 +56,44 @@ export default function Register(){
 
     }
 
+    const handleDelete = async ()=>{
+        try {
+            if(!id){
+                toast.error("Ação não permitida")
+            }
+
+            await axios.delete('/users')
+
+            toast.success('Conta apagada com sucesso!');
+
+            dispatch(actions.LoginFailure())
+
+        } catch (err) {
+            const errors = get(err, 'response.data.errors', [])
+            const status = get(err, 'response.status', 0)
+        
+            if(status === 400){
+              toast.error('Você precisa fazer o login novamente')
+              dispatch(actions.LoginFailure())
+              window.location.href = '/login'
+              return 
+            }
+
+            if(errors.length > 0){
+                errors.map(error => toast.error(error))
+            }else{
+                toast.error('Erro desconhecido')
+            }
+        }
+
+    } 
+
     return(
         <Container>
             <Loading isLoading={isLoading}/>
 
-            <h1 style={{textAlign:'center'}}>{id ? 'Editar conta' :'Registro'}</h1>
+            <h1 style={{textAlign:'center',marginBottom:'30px'}}>{id ? 'Editar conta' :'Registro'}</h1>
+            {id && <a style={{cursor:'pointer'}} onClick={handleDelete}>Apagar conta</a>}
 
             <Form onSubmit={handleSubmit}>
                 <input type='text' 
